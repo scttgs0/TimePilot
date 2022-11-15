b47934d2da01d9392fb51257f65ed71ee4b20f1d
Scott Giese <scttgs0@gmail.com>
2022-11-15 01:07:47 -0600

    refactor loader

--- loader/loadGameGraphic.asm
+++ loader/loadGameGraphic.asm
@@ -1,120 +1,159 @@
+
 ; ----------------------------------
-; 			DATA LOADER
-;	copy gfx data to RAM under ROM
+;             DATA LOADER
+;    copy gfx data to RAM under ROM
 ;-----------------------------------
 
-	org $2000
-copyFm		equ $3000	; $1000
-copyFm2		equ $4000	; $1000
-copyFm3		equ $5000	; $400
-copyFm4		equ $5400	; $200
+;--------------------------------------
+;--------------------------------------
+                * = $2000
+;--------------------------------------
 
-	jsr loader.disableOS
+copyFm          = $3000               ; $1000
+copyFm2         = $4000               ; $1000
+copyFm3         = $5000               ; $400
+copyFm4         = $5400               ; $200
 
-	; gameGraphic
-	.macro copyDataObjects 
-	lda copyFm+$000+(:1<<8),x
-	sta dataGameGraphic+$000+(:1<<8),x
-	.endm
-	ldx #0
-@	.rept 16
-	copyDataObjects .R
-	.endr
-	inx
-	bne @-
-	
-	; gameGraphic2	
-	.macro copyDataObjects2
-	lda copyFm2+$000+(:1<<8),x
-	sta dataGameGraphic2+$000+(:1<<8),x
-	.endm
-	ldx #0
-@	.rept 16
-	copyDataObjects2 .R
-	.endr
-	inx
-	bne @-
+                jsr loader.disableOS
 
-	; gameGraphic3
-	.macro copyDataObjects3
-	lda copyFm3+$000+(:1<<8),x
-	sta dataLogoFonts+$000+(:1<<8),x
-	.endm
-	ldx #0
-@	.rept 4
-	copyDataObjects3 .R
-	.endr
-	inx
-	bne @-
-	
-	; gameGraphic4	(clouds, progress bars)
-	.macro copyDataObjects4
-	lda copyFm4+$000+(:1<<8),x
-	sta dataCloudSmall+$000+(:1<<8),x
-	.endm
-	ldx #0
-@	.rept 2
-	copyDataObjects4 .R
-	.endr
-	inx
-	bne @-	
 
-	; determines PAL or NTSC
-	ldx $d014
-	dex
-	beq @+
-	ldx #7
-@	
-	stx $f600	; game reads it and store information on zero page
-	
-	; finish	
-	jmp loader.enableOS
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+; gameGraphic
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+copyDataObjects .macro
+                lda copyFm+$000+(\1<<8),x
+                sta dataGameGraphic+$000+(\1<<8),x
+                .endmacro
 
-	icl 'loader/loaderProcedures.asm'
-	
-	org copyFm
-	ins 'data/graphic/enemies/enemyLevel1.dat'
-	ins 'data/graphic/enemies/enemyLevel2.dat'
-	ins 'data/graphic/enemies/enemyLevel4.dat'
-	ins 'data/graphic/enemies/bossLevel1.dat'
-	ins 'data/graphic/enemies/bossLevel2.dat'
-	ins 'data/graphic/enemies/bossLevel3.dat'
-	ins 'data/graphic/enemies/bossLevel4.dat'
 
-	org copyFm2
-	ins 'data/graphic/enemies/enemyLevel3.dat'
-	ins 'data/graphic/enemies/enemyLevel5.dat'
-	ins 'data/graphic/enemies/enemyExplosion.dat'
-	ins 'data/graphic/bonuses/parachute.dat'
-	
-	ins 'data/graphic/enemies/bossLevel5.dat'
-	ins 'data/graphic/player.raw'
-	ins 'data/graphic/asteroids/asteroidSmall.dat'
-	ins 'data/graphic/asteroids/asteroidMedium.dat'
-	ins 'data/graphic/asteroids/asteroidBig.dat'
-	ins 'data/graphic/enemies/bomb.dat'
-	
-	org copyFm2+(dataTextFonts-dataGameGraphic2)
-	ins 'data/graphic/timePilotFonts.fnt'
-	ins 'data/graphic/enemies/bomb_lvl5.dat'
-	ins 'data/graphic/bonuses/cosmonaut.dat'
-	
-	org copyFm3
-	ins 'data/graphic/titleScreenLogo.fnt'
-	
-	org copyFm4
-	ins 'data/graphic/clouds/cloudSmall.dat'
-	ins 'data/graphic/clouds/cloudMedium.dat'
-	
-	ins 'data/graphic/progressBar/levelBar1.dat'
-	ins 'data/graphic/progressBar/levelBar2.dat'
-	ins 'data/graphic/progressBar/levelBar3.dat'
-	ins 'data/graphic/progressBar/levelBar4.dat'
-	ins 'data/graphic/progressBar/levelBar5.dat'
-	ins 'data/graphic/clouds/cloudBig.dat'
-	
+                ldx #0
+_next1      .for item in range(16)
+                .copyDataObjects item
+            .endfor
 
-	
-	
+                inx
+                bne _next1
 
-	
+
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+; gameGraphic2
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+copyDataObjects2 .macro
+                lda copyFm2+$000+(\1<<8),x
+                sta dataGameGraphic2+$000+(\1<<8),x
+                .endmacro
+
+
+                ldx #0
+_next2      .for item in range(16)
+                .copyDataObjects2 item
+            .endfor
+
+                inx
+                bne _next2
+
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+; gameGraphic3
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+copyDataObjects3 .macro
+                lda copyFm3+$000+(\1<<8),x
+                sta dataLogoFonts+$000+(\1<<8),x
+                .endmacro
+
+
+                ldx #0
+_next3      .for item in range(4)
+                .copyDataObjects3 item
+            .endfor
+
+                inx
+                bne _next3
+
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+; gameGraphic4    (clouds, progress bars)
+;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+copyDataObjects4 .macro
+                lda copyFm4+$000+(\1<<8),x
+                sta dataCloudSmall+$000+(\1<<8),x
+                .endmacro
+
+
+                ldx #0
+_next4      .for item in range(2)
+                .copyDataObjects4 item
+            .endfor
+
+                inx
+                bne _next4
+
+; determines PAL or NTSC
+                ldx $d014
+                dex
+                beq _5
+
+                ldx #7
+_5              stx $f600               ; game reads it and store information on zero page
+
+; finish
+                jmp loader.enableOS
+
+
+;--------------------------------------
+                .include 'loaderProcedures.asm'
+;--------------------------------------
+
+;--------------------------------------
+;--------------------------------------
+                * = copyFm
+;--------------------------------------
+                .binary '../data/graphic/enemies/enemyLevel1.dat'
+                .binary '../data/graphic/enemies/enemyLevel2.dat'
+                .binary '../data/graphic/enemies/enemyLevel4.dat'
+                .binary '../data/graphic/enemies/bossLevel1.dat'
+                .binary '../data/graphic/enemies/bossLevel2.dat'
+                .binary '../data/graphic/enemies/bossLevel3.dat'
+                .binary '../data/graphic/enemies/bossLevel4.dat'
+
+;--------------------------------------
+;--------------------------------------
+                * = copyFm2
+;--------------------------------------
+                .binary '../data/graphic/enemies/enemyLevel3.dat'
+                .binary '../data/graphic/enemies/enemyLevel5.dat'
+                .binary '../data/graphic/enemies/enemyExplosion.dat'
+                .binary '../data/graphic/bonuses/parachute.dat'
+
+                .binary '../data/graphic/enemies/bossLevel5.dat'
+                .binary '../data/graphic/player.raw'
+                .binary '../data/graphic/asteroids/asteroidSmall.dat'
+                .binary '../data/graphic/asteroids/asteroidMedium.dat'
+                .binary '../data/graphic/asteroids/asteroidBig.dat'
+                .binary '../data/graphic/enemies/bomb.dat'
+
+;--------------------------------------
+;--------------------------------------
+                * = copyFm2+(dataTextFonts-dataGameGraphic2)
+;--------------------------------------
+                .binary '../data/graphic/timePilotFonts.fnt'
+                .binary '../data/graphic/enemies/bomb_lvl5.dat'
+                .binary '../data/graphic/bonuses/cosmonaut.dat'
+
+;--------------------------------------
+;--------------------------------------
+                * = copyFm3
+;--------------------------------------
+                .binary '../data/graphic/titleScreenLogo.fnt'
+
+;--------------------------------------
+;--------------------------------------
+                * = copyFm4
+;--------------------------------------
+                .binary '../data/graphic/clouds/cloudSmall.dat'
+                .binary '../data/graphic/clouds/cloudMedium.dat'
+
+                .binary '../data/graphic/progressBar/levelBar1.dat'
+                .binary '../data/graphic/progressBar/levelBar2.dat'
+                .binary '../data/graphic/progressBar/levelBar3.dat'
+                .binary '../data/graphic/progressBar/levelBar4.dat'
+                .binary '../data/graphic/progressBar/levelBar5.dat'
+                .binary '../data/graphic/clouds/cloudBig.dat'
--- loader/loaderProcedures.asm
+++ loader/loaderProcedures.asm
@@ -1,27 +1,35 @@
 
+loader          .namespace
 
 
-.proc 	loader
-disableOS
-    sei
-    lda #0
-    sta nmien      
-    lda portb
-    ldy #%11111110
-    sty portb       
-    and #%11111110  
-    sta portb       
-    rts
+;======================================
+;
+;======================================
+disableOS       sei
+                lda #0
+                sta nmien
 
-enableOS    
-	lda portb
-    ldy #%11111111
-    sty portb       
-    ora #%00000001  
-    sta portb       
-    lda #%11000000  
-    sta nmien       
-    cli
-    rts   		
-.endp
+                lda portb
+                ldy #%11111110
+                sty portb
+                and #%11111110
+                sta portb
+                rts
 
+
+;======================================
+;
+;======================================
+enableOS        lda portb
+                ldy #%11111111
+                sty portb
+                ora #%00000001
+                sta portb
+
+                lda #%11000000
+                sta nmien
+
+                cli
+                rts
+
+                .endnamespace
